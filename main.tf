c68eb405e73d05b615910a09e917824e795a24e0
Condensed-milk <s210ca.kt@gmail.com>
2023-03-19 23:10:57 +0900

    テスト用S3バケット作成

---
+++ main.tf
@@ -0,0 +1,22 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region = "ap-northeast-1"
+}
+
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "test-s3-acl" {
+  bucket = aws_s3_bucket.test-bucket.id
+  acl    = "private"
+}
